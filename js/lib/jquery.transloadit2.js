e6eee590eadfd4c742ebc0b65273854f25c532f0
tim-kos <tim@debuggable.com>
2013-07-31 16:42:52 +0200

    change copyright info

--- js/lib/jquery.transloadit2.js
+++ js/lib/jquery.transloadit2.js
@@ -1,4 +1,4 @@
-/** @license jquery.transloadit2.js: Copyright (c) 2010 Felix Geisendörfer | MIT License: http://www.opensource.org/licenses/mit-license.php
+/** @license jquery.transloadit2.js: Copyright (c) 2013 Transloadit Ltd | MIT License: http://www.opensource.org/licenses/mit-license.php
  *
  * Fork this on Github: http://github.com/transloadit/jquery-sdk
  *
