edd82c5af920837188cc604c204427b6cbc54e5b
tim-kos <tim@debuggable.com>
2020-06-30 07:21:26 +0200

    dd back logging.

--- tests/system/test-trigger-upload-on-file-selection.js
+++ tests/system/test-trigger-upload-on-file-selection.js
@@ -1,3 +1,5 @@
+const util = require('util')
+
 const numberOfPlannedTests = 6
 casper.test.begin('test-trigger-upload-on-file-selection', numberOfPlannedTests, (test) => {
   casper.start(`http://${testhost}/trigger-on-file-select`, function () {
@@ -16,6 +18,7 @@ casper.test.begin('test-trigger-upload-on-file-selection', numberOfPlannedTests,
   })
 
   casper.then(function () {
+    console.log(util.inspect(this.test))
     this.test.assertTextExists('"ok":"ASSEMBLY_COMPLETED"')
   })
 
